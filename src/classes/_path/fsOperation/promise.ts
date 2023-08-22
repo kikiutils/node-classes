97a31a8f0c2586b641864683c525b35fcc2cca49
kiki-kanri <a470666@gmail.com>
2023-08-22 14:13:27 +0800

    feat: add path fs operation classes

---
+++ src/classes/_path/fsOperation/promise.ts
@@ -0,0 +1,58 @@
+import kFse from '@kikiutils/fs-extra';
+import fsp from 'fs/promises';
+import fse, { EnsureDirOptions } from 'fs-extra';
+
+import { BasePath } from '../base';
+
+export class PromisePathFsOperation extends BasePath {
+	/**
+	 * @see {@link fsp.appendFile}
+	 */
+	async appendFile(data: string | Uint8Array, options?: BufferEncoding | (fse.ObjectEncodingOptions & fsp.FlagAndOpenMode) | null) {
+		return await kFse.appendFile(this.raw, data, options);
+	}
+
+	/**
+	 * @see {@link fse.emptyDir}
+	 */
+	async emptyDir() {
+		return await kFse.emptyDir(this.raw);
+	}
+
+	/**
+	 * @see {@link fse.mkdirs}
+	 */
+	async mkdirs(options?: EnsureDirOptions | number) {
+		return await kFse.mkdirs(this.raw, options);
+	}
+
+	/**
+	 * Check path is dir.
+	 */
+	async isDir() {
+		return (await kFse.stat(this.raw))?.isDirectory() || false;
+	}
+
+	/**
+	 * Check path is file.
+	 */
+	async isFile() {
+		return (await kFse.stat(this.raw))?.isFile() || false;
+	}
+
+	/**
+	 * @see {@link fse.remove}
+	 */
+	async remove() {
+		return await kFse.remove(this.raw);
+	}
+
+	/**
+	 * @see {@link fsp.truncate}
+	 */
+	async truncate(length?: number) {
+		return await kFse.truncate(this.raw, length);
+	}
+}
+
+export default PromisePathFsOperation;
---
+++ src/classes/_path/fsOperation/sync.ts
@@ -0,0 +1,56 @@
+import kFse from '@kikiutils/fs-extra';
+import fs from 'fs';
+import fse, { EnsureDirOptions } from 'fs-extra';
+
+import PromisePathFsOperation from './promise';
+
+export class SyncPathFsOperation extends PromisePathFsOperation {
+	/**
+	 * @see {@link fs.appendFileSync}
+	 */
+	appendFileSync(data: string | Uint8Array, options?: fs.WriteFileOptions) {
+		return kFse.appendFileSync(this.raw, data, options);
+	}
+
+	/**
+	 * @see {@link fse.emptyDirSync}
+	 */
+	emptyDirSync() {
+		return kFse.emptyDirSync(this.raw);
+	}
+
+	/**
+	 * @see {@link fse.mkdirsSync}
+	 */
+	mkdirsSync(options?: EnsureDirOptions | number) {
+		return kFse.mkdirsSync(this.raw, options);
+	}
+
+	/**
+	 * Check path is dir.
+	 */
+	isDirSync() {
+		return kFse.statSync(this.raw)?.isDirectory() || false;
+	}
+
+	/**
+	 * Check path is file.
+	 */
+	isFileSync() {
+		return kFse.statSync(this.raw)?.isFile() || false;
+	}
+
+	/**
+	 * @see {@link fse.removeSync}
+	 */
+	removeSync() {
+		return kFse.removeSync(this.raw);
+	}
+
+	/**
+	 * @see {@link fs.truncateSync}
+	 */
+	truncateSync(length?: number) {
+		return kFse.truncateSync(this.raw, length);
+	}
+}
