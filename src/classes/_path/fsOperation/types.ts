761b5fd3eacaf222a50a4a30e53a5c2072091d56
kiki-kanri <a470666@gmail.com>
2023-08-23 04:29:30 +0800

    feat: add kFse paramters type

--- src/classes/_path/fsOperation/types.ts
+++ src/classes/_path/fsOperation/types.ts
@@ -1,14 +1,5 @@
-import { Abortable } from 'events';
-import { Mode, ObjectEncodingOptions, OpenMode } from 'fs';
-import { Stream } from 'stream';
+import kFse from '@kikiutils/fs-extra';
 
-export type ReadFileOptions = BufferEncoding | (ObjectEncodingOptions & Abortable & { flag?: OpenMode; }) | null;
-export type ReadFileSyncOptions = BufferEncoding | (ObjectEncodingOptions & { flag?: string; }) | null;
-export type WriteFileData = string | NodeJS.ArrayBufferView | Iterable<string | NodeJS.ArrayBufferView> | AsyncIterable<string | NodeJS.ArrayBufferView> | Stream;
-export type WriteFileOptions =
-	BufferEncoding
-	| (ObjectEncodingOptions & {
-		flag?: OpenMode;
-		mode?: Mode;
-	} & Abortable)
-	| null;
+export type KFseParameters = {
+	[K in keyof typeof kFse]: Parameters<typeof kFse[K]>;
+}
