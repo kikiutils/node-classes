6f496a4f9fe3b3235efe25e04292a3b387cb1ead
kiki-kanri <a470666@gmail.com>
2023-08-23 18:14:06 +0800

    feat: add isDir and isFile methods

--- src/classes/_path/fsOperation/index.ts
+++ src/classes/_path/fsOperation/index.ts
@@ -1,8 +1,34 @@
+import { PathLike } from '../base';
 import PathFseOperation from './_fsExtra';
 
 /**
- * All fs operations of native fs, fs/promises and fs-extra methods.
+ * All fs operations of custom operation, native fs, fs/promises and fs-extra methods.
  */
-export const PathFsaOperation = PathFseOperation;
+export class PathFsaOperation extends PathFseOperation {
+	isDir: typeof this.isDirectory;
+	isDirSync: typeof this.isDirectorySync;
+
+	constructor(...paths: PathLike[]) {
+		super(...paths);
+		this.isDir = this.isDirectory;
+		this.isDirSync = this.isDirectorySync;
+	}
+
+	async isDirectory() {
+		return (await this.stat())?.isDirectory() || false;
+	}
+
+	isDirectorySync() {
+		return this.statSync()?.isDirectory() || false;
+	}
+
+	async isFile() {
+		return (await this.stat())?.isFile() || false;
+	}
+
+	isFileSync() {
+		return this.statSync()?.isFile() || false;
+	}
+}
 
 export default PathFsaOperation;
