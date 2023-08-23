9383bd14baa615b94c353d0f8b970461db3ff753
kiki-kanri <a470666@gmail.com>
2023-08-23 18:36:38 +0800

    feat: Path class add readFileToBlob method

--- src/classes/_path/fsOperation/index.ts
+++ src/classes/_path/fsOperation/index.ts
@@ -1,5 +1,8 @@
+import fs from 'fs';
+
 import { PathLike } from '../base';
 import PathFseOperation from './_fsExtra';
+import { KFseParameters } from './types';
 
 /**
  * All fs operations of custom operation, native fs, fs/promises and fs-extra methods.
@@ -28,6 +31,16 @@ export class PathFsaOperation extends PathFseOperation {
 
 	isFileSync() {
 		return this.statSync()?.isFile() || false;
+	}
+
+	async readFileToBlob(options?: KFseParameters['readFile'][1]) {
+		const file = await this.readFile(options);
+		if (file) return new Blob([file]);
+	}
+
+	readFileToBlobSync(options?: Parameters<typeof fs.readFileSync>[1]) {
+		const file = this.readFileSync(options);
+		if (file) return new Blob([file]);
 	}
 }
 
